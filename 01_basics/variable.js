b85726de5c71da4cf5130d010e22b77753a9e942
bisenanshu <111082420+bisenanshu@users.noreply.github.com>
2023-10-02 10:14:01 +0000

    change new

---
+++ 01_basics/variable.js
@@ -0,0 +1,31 @@
+//there is two of data type
+//primitive data type and non-primitive data type
+//primitive data type
+//String , symbole , number , null , boolean , bigInt , undefined (stack memory) ||
+//string , array , function (heap )
+// let score = "Anshu";
+// let scoree = score;
+// console.log(score);
+// console.log(scoree);
+// scoree = "AnshuBisen";
+// console.log(scoree);
+// console.log(score);
+
+const stu = {
+    name : "Anshu",
+    class : 12 , 
+    gmail : "bisenass@gmail.com"
+}
+// console.log(stu);
+// console.log(typeof stu);
+// stu.gmail = "anshubisen@gmail.com";
+// console.log(stu);
+
+let sco = [1, "Anshu" , 123 , undefined , null]
+// console.log(sco);
+// console.log(typeof sco);
+
+function info(){
+ console.log("Anshu bisen");
+}
+info();
