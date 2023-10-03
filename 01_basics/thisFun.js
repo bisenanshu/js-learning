b6a9a7762fa27a174d6c20c2b9f721737969ca9c
bisenanshu <111082420+bisenanshu@users.noreply.github.com>
2023-10-03 09:05:52 +0000

    add new jsfile

---
+++ 01_basics/thisFun.js
@@ -0,0 +1,16 @@
+// const obj = {
+//     Name : "Anshu Bisen",
+//     Gmail : "anshubisen45@gmail.com",
+//     RollNo : 123,
+//     myFun : function(){
+//         console.log(`${this.Name} ,wellcome to this website`);
+//     }
+// }
+// obj.myFun();
+// obj.Name = "Anshuman" ;
+// obj.myFun();
+
+//console.log(this);  //print ==> {}
+
+
+//function and arr function()
