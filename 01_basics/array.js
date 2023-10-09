79d8c3d622e2b4556fecba4166c23798dd38510f
bisenanshu <111082420+bisenanshu@users.noreply.github.com>
2023-10-09 03:53:33 +0000

    Array

---
+++ 01_basics/array.js
@@ -0,0 +1,33 @@
+let arr = [1,2,3,4,5,6,7,8,9];
+let arr1 = [10,11,12];
+let newArr = arr.concate(arr1);
+console.log(newArr);
+
+// let newArr = arr.toString();
+// console.log(newArr)
+// console.log(typeof newArr);
+// console.log(arr);
+
+// let newArr = arr.join("_");
+// console.log(newArr);
+// console.log(arr);
+
+// arr.unshift(0);
+// console.log(arr);
+// arr.shift()
+// console.log(arr);
+
+// arr.push(10);
+// console.log(arr);
+// arr.pop();
+// console.log(arr);
+// console.log(arr);
+// console.log(typeof arr);
+// console.log(arr[1]);
+// let lnt = arr.length
+// console.log(lnt);
+
+// let str = "Anshu";
+// console.log(typeof str);
+// let newStr = Array.from(str);
+// console.log(typeof newStr);
