e3f6bb8750f2b478a7791ad14e93d9bde25220d8
bisenanshu <111082420+bisenanshu@users.noreply.github.com>
2023-10-09 05:10:36 +0000

    array

--- 01_basics/array.js
+++ 01_basics/array.js
@@ -1,7 +1,25 @@
-let arr = [1,2,3,4,5,6,7,8,9];
-let arr1 = [10,11,12];
-let newArr = arr.concate(arr1);
-console.log(newArr);
+let arr = [1,2,3,4,5,6,7,8,99];
+
+// console.log(arr.includes(99));
+// console.log(arr.indexOf(99))
+
+// let newArr = arr.slice(3);
+// console.log(newArr);
+
+// arr.splice(2,0,111,222,333,444)
+// console.log(arr);
+
+// let bacheElements = arr.splice(2,3,2022,2023,2024,2025);
+// console.log(arr);
+// console.log(bacheElements);
+
+//  delete arr[0] ;
+// console.log(arr);
+
+// let arr1 = [10,11,12];
+// let arr2 = [113,14];
+// let newArr = arr.concat(arr1,arr2);
+// console.log(newArr);
 
 // let newArr = arr.toString();
 // console.log(newArr)
