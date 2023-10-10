439f02c9ddbf44cb305241d6d8c73a425e94d7cf
bisenanshu <111082420+bisenanshu@users.noreply.github.com>
2023-10-10 09:16:59 +0000

    obj1

---
+++ 01_basics/obj1.js
@@ -0,0 +1,14 @@
+//singleton object
+
+const obj = new Object();
+console.log(obj);
+
+const objj = {}
+console.log(obj1);
+objj.name = "Anshu";
+objj.surname = "Bisen";
+objj.age = 22;
+objj.clg = "SRIT";
+objj.batch = 2023;
+
+
