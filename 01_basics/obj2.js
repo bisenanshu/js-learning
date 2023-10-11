deea0756e0b3f5ed12d5e5707c135be6af7435b9
bisenanshu <111082420+bisenanshu@users.noreply.github.com>
2023-10-11 04:27:32 +0000

    json

---
+++ 01_basics/obj2.js
@@ -0,0 +1,28 @@
+//*************Object de-structure******** */
+
+const course = {
+    coursename : "js",
+    feesh : 999,
+    coursInstructer : "Anshu"
+}
+
+//const {coursInstructer} = course
+//console.log(coursInstructer);
+const {coursename : name } = course
+console.log(name);
+
+//********************************* */
+    //****web site / 
+
+
+    //random user me api
+   //json formatte
+
+//json ==> js objnect notation
+
+//{
+//   "name"  :"ansu",  
+// "id"   : 123 ,
+//     "condition" : true,
+//     "call" : " srid"
+// }
