3fcaf1be9fba31de2b13d09fcb5b62ba2b1fe96f
bisenanshu <111082420+bisenanshu@users.noreply.github.com>
2023-10-18 04:40:05 +0000

    insert data

---
+++ 01_basics/highOrd.js
@@ -0,0 +1,87 @@
+// let arr = [12,22,33,44,55,66];
+// for(let val of arr){
+//     console.log(val);
+// }   ==> print array value
+
+// for(let val in arr){
+//     console.log(val);
+// }   ==> print array keys
+
+//#########################################
+
+
+// const object = {
+//     name : "Anshu Bisen",
+//     age : 32,
+//     batch : 2023
+// }
+
+//for(let obj in object){
+//  console.log(obj ,"=>", object[obj] );
+// }
+
+//######################################
+
+//===========(for in loop)=============
+
+const information = {
+    name : "anshu",
+    dep  : "IT",
+    rollNo : 8,
+    batch : 2023
+}
+ for(let info in information){
+  //console.log(info,"=>",information[info]);
+ }
+
+
+
+ let number = [12,33,22,33,45,56];
+ for(let num in number){
+//    console.log(num ,"=>",number[num]);
+ }
+
+
+
+ let greeting = "Good morning";
+ for(let greet in greeting){
+   // console.log(greeting[greet]);
+ }
+
+
+ //##########################################
+
+
+ //==========(for of loop)=================
+
+//  #####################################
+//  const information1 = {              #
+//     name  : "anshu",                 #
+//     dep   : "IT",                    #
+//     batch : 2023,                    #
+//  }                                   #
+// for(let info1 of information1){      #
+//     console.log(info1);              #
+// }                                    #
+//for of loop is not work in obj i      #
+//dont know why                         #
+// ######################################
+
+let number1 = [12,22,33,44,55,66]
+for(let num1 of number1){
+    //console.log(num1);
+}
+
+let greeting1 = "Good Night";
+for(let greet1 of greeting1){
+   // console.log(greet1);
+}
+
+
+
+
+
+
+
+
+
